268df953023e35fa2206c1704de0d0e85bda9067
Vikash Yadav <vikash@citrusplatform.com>
2018-06-21 13:51:25 +0530

    Updated few users

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,7 +16,7 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
     router.get("json") { req in
-        return [Person(name: "INSHU DUDEJA", age: 26 ,gender:"female" , imageFileId:"1_1_7_G_I_I3_f15gfw42v4" ,mobileNumber:"91-9717098526",userName:"9197ajukt611985"),Person(name: "Aman", age: 26 ,gender:"female" , imageFileId:"1_1_7_G_I_I3_eyow4il4rh" ,mobileNumber:"91-8288011172",userName:"918231t90ujj111"),Person(name: "Rajiv", age: 26 ,gender:"male" , imageFileId:"1_1_7_G_I_I3_f19rwfpxa0" ,mobileNumber:"1-6195504830",userName:"1619u5rlvml6048")]
+        return [Person(name: "Tariq", age: 26 ,gender:"female" , imageFileId:"1_1_7_G_I_I3_f25oekursi" ,mobileNumber:"91-5901712017",userName:"9159yfdo14tv120"),Person(name: "Musa Bhai", age: 26 ,gender:"female" , imageFileId:"1_1_7_G_I_I3_f25oekursj" ,mobileNumber:"91-4901712017",userName:"9149p3g0fsjk120"),Person(name: "Musa Bhai", age: 26 ,gender:"male" , imageFileId:"1_1_7_G_I_I3_f25oekursl" ,mobileNumber:"91-3901712017",userName:"9139c0ux9vcr120"),Person(name: "Rajiv", age: 26 ,gender:"male" , imageFileId:"1_1_7_G_I_I3_f25oekursp" ,mobileNumber:"91-2901712017",userName:"9129tsbystl2120"),Person(name: "Swati Thapa", age: 26 ,gender:"male" , imageFileId:"1_1_7_G_I_I3_f25oekurss" ,mobileNumber:"91-1101712017",userName:"9111u8mg4yz4120"),Person(name: "Mahesh", age: 26 ,gender:"male" , imageFileId:"1_1_7_G_I_I3_f25oekursu" ,mobileNumber:"91-1111712017",userName:"9111uetlsqht120")]
     }
     // Example of configuring a controller
     let todoController = TodoController()
